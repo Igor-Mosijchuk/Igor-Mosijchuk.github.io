1c47d8aeb7cf7dde97fabd12d11d25383426ad4a
igor <igor2wdev@gmail.com>
2019-01-08 02:02:51 +0200

    smartgrid delete version

--- 2019/m01-Layout/asos/gulpfile.js
+++ 2019/m01-Layout/asos/gulpfile.js
@@ -8,19 +8,56 @@ var gulp	= require('gulp'),
 	prefix	= require('gulp-autoprefixer'),
 	pug	= require('gulp-pug'),
 	plumber = require('gulp-plumber'),
+	smartgrid = require('smart-grid'),
 	sass	= require('gulp-sass'),
 	srcmaps	= require('gulp-sourcemaps'),
 	paths	= {
 		blocks: 'blocks/',
 		devDir: 'app/',
 		outputDir: 'build/'
-	};
+	},
+	settings = {
+		outputStyle: 'sass', /* less || scss || sass || styl */
+		columns: 12, /* number of grid columns */
+		offset: '32px', /* gutter width px || % || rem */
+		mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+		container: {
+			maxWidth: '1170px', /* max-width оn very large screen */
+			fields: '68px' /* side fields */
+		},
+		breakPoints: {
+			lg: {
+				width: '1100px', /* -> @media (max-width: 1100px) */
+			},
+			md: {
+				width: '960px'
+			},
+			sm: {
+				width: '780px',
+				fields: '15px' /* set fields only if you want to change container.fields */
+			},
+			xs: {
+				width: '560px'
+			}
+			/* 
+			We can create any quantity of break points.
+ 
+			some_name: {
+				 width: 'Npx',
+				 fields: 'N(px|%|rem)',
+				 offset: 'N(px|%|rem)'
+			}
+			*/
+		}
+	}
+	;
+smartgrid('blocks/sass/', settings)
 
 //	BROWSER SYNC
 gulp.task('serve', function() {
 	browserSync.init({
 		server: {
-			baseDir: "./app"
+			baseDir: "app"
 		},
 		notify: false
 	})
