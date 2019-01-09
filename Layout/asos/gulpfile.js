f86760edd351c37c06058fa114d2c1944534c3a5
igor <igor2wdev@gmail.com>
2019-01-09 13:14:52 +0200

    success col&box mixin

--- Layout/asos/gulpfile.js
+++ Layout/asos/gulpfile.js
@@ -15,43 +15,7 @@ var gulp	= require('gulp'),
 		blocks: 'blocks/',
 		devDir: 'app/',
 		outputDir: 'build/'
-	},
-	settings = {
-		outputStyle: 'sass', /* less || scss || sass || styl */
-		columns: 12, /* number of grid columns */
-		offset: '32px', /* gutter width px || % || rem */
-		mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
-		container: {
-			maxWidth: '1170px', /* max-width оn very large screen */
-			fields: '15px' /* side fields */
-		},
-		breakPoints: {
-			lg: {
-				width: '1100px', /* -> @media (max-width: 1100px) */
-			},
-			md: {
-				width: '960px'
-			},
-			sm: {
-				width: '780px',
-				fields: '15px' /* set fields only if you want to change container.fields */
-			},
-			xs: {
-				width: '560px'
-			}
-			/* 
-			We can create any quantity of break points.
- 
-			some_name: {
-				 width: 'Npx',
-				 fields: 'N(px|%|rem)',
-				 offset: 'N(px|%|rem)'
-			}
-			*/
-		}
-	}
-	;
-smartgrid('blocks/sass/', settings)
+	};
 
 //	BROWSER SYNC
 gulp.task('serve', function() {
